7d97cd6116406a7f6f3928bb25c5891c3bdd2169
Oussama El Aloui <OussezScripter@gmail.com>
2022-06-03 11:23:07 +0100

    last change

--- tests/user.php
+++ tests/user.php
@@ -1,6 +1,8 @@
 <?php 
 
-class User {
+use PHPUnit\Framework\TestCase;
+
+class User extends TestCase{
 
     public $username;
     public $email;
@@ -12,14 +14,36 @@ class User {
     }
 
     public function addFriend(){
-      //return "added a new friend";
-      return "$this->username just added a new friend";
+      
+      return "\n[**]$this->username just added a new friend\n";
+    }
+
+    public function getUname(){
+      
+      return $this->username;
+    }
+
+    public function getMail(){
+      
+      return $this->email;
+    }
+    public function testName()
+    {
+        $this->assertSame('Oussez', $this->username);
+    }
+
+    public function testMail()
+    {
+        $this->assertSame('Oussez@gmail.com', $this->email);
     }
 
   }
 
-  $userOne = new User('Oussez12', 'mario@thenetninja.co.uk');
+  $userOne = new User('Oussez', 'Oussez@gmail.com');
 
   echo $userOne->addFriend();
-
+  
+  if($userOne->testMail() || $userOne->testUname())
+    {echo "[***] Username & Email are correctly matched";}
+  //$this->assertSame('Oussez@gmail.com',$userOne->getMail());
 ?>
